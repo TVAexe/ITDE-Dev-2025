bdf28e85f9f2c8e07e66dfe9754cfd2d979678fa
lehuuchinh <chinh.lh54@gmail.com>
2024-11-26 20:32:14 +0700

    start

---
+++ src/main/java/vn/lehuuchinh/jobhunter/ultil/error/IdInvalidException.java
@@ -0,0 +1,8 @@
+package vn.lehuuchinh.jobhunter.ultil.error;
+
+public class IdInvalidException extends Exception {
+    // Constructor that accepts a message
+    public IdInvalidException(String message) {
+        super(message);
+    }
+}
