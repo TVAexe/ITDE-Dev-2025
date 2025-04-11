36dbb1e56d0828fe5ed146e8abaaec7cb0c933ce
Ninh Hai Nam <ninhhainam12@gmail.com>
2025-04-11 13:56:21 +0700

    fix

--- frontend-core-web/src/services/api.tsx
+++ frontend-core-web/src/services/api.tsx
@@ -6,7 +6,7 @@ const apiSlice = createApi({
     baseUrl: 'http://localhost:5000',
     prepareHeaders: async (headers) => {
       try {
-        const token = localStorage.getItem('accessToken') || "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJuZXd1c2VyIiwiY2hpbmgubGg1NCI6eyJwcmluY2lwYWwiOnsicGFzc3dvcmQiOm51bGwsInVzZXJuYW1lIjoibmV3dXNlciIsImF1dGhvcml0aWVzIjpbeyJyb2xlIjoiUk9MRV9VU0VSIn1dLCJhY2NvdW50Tm9uRXhwaXJlZCI6dHJ1ZSwiYWNjb3VudE5vbkxvY2tlZCI6dHJ1ZSwiY3JlZGVudGlhbHNOb25FeHBpcmVkIjp0cnVlLCJlbmFibGVkIjp0cnVlfSwiY3JlZGVudGlhbHMiOm51bGwsImF1dGhvcml0aWVzIjpbeyJyb2xlIjoiUk9MRV9VU0VSIn1dLCJkZXRhaWxzIjpudWxsLCJhdXRoZW50aWNhdGVkIjp0cnVlfSwiZXhwIjoxMDM4MzY4MjUxNSwiaWF0IjoxNzQzNjgyNTE1fQ.1JVq1VbsbMDDGMJ4hBI4xFQiNSuvmG76BNliSJl52zj7a26asxGC_BC94hAibWAlXp949cRkqUm2tPLzRnlElA";
+        const token = localStorage.getItem('accessToken');
         if (token) {
           headers.set('authorization', `Bearer ${token}`);
         }
